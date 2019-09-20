b745d4881c613ac89a9415302bd48e91691b5b53
Bharatsewani1993 <bharatsewani1993@gmail.com>
2019-09-20 20:30:57 +0530

    Resize profile pic, Convert to circle shape, added text to image and final frame is ready

--- main.go
+++ main.go
@@ -9,10 +9,18 @@ import (
 	"strconv"
 	"strings"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/jpeg"
 	"image/png"
+	"log"
 	"github.com/ChimeraCoder/anaconda"
+	"github.com/disintegration/imaging"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+    "golang.org/x/image/font/inconsolata"
+	"golang.org/x/image/math/fixed"
+	"reflect"
 )
 
 //TwitterLogin is used for authentication purpose
@@ -79,37 +87,125 @@ func IsURL(str string) bool {
 	return err == nil && u.Scheme != "" && u.Host != ""
 }
 
+
+type circle struct {
+	p image.Point
+	r int
+}
+
+func (c *circle) ColorModel() color.Model {
+	return color.AlphaModel
+}
+
+func (c *circle) Bounds() image.Rectangle {
+	return image.Rect(c.p.X-c.r, c.p.Y-c.r, c.p.X+c.r, c.p.Y+c.r)
+}
+
+func (c *circle) At(x, y int) color.Color {
+	xx, yy, rr := float64(x-c.p.X)+0.5, float64(y-c.p.Y)+0.5, float64(c.r)
+	if xx*xx+yy*yy < rr*rr {
+		return color.Alpha{255}
+	}
+	return color.Alpha{0}
+}
+
+//Write text on image
+func AddLabel(img *image.RGBA, x, y int, label string, fontface *basicfont.Face, col color.RGBA) {   
+    point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
+
+    d := &font.Drawer{
+        Dst:  img,
+        Src:  image.NewUniform(col),
+        Face: fontface,
+        Dot:  point,
+    }
+    d.DrawString(label)
+}
+
 //CreateImages create images for video
 func CreateImages() {
-	backGround, err := os.Open("background.png")
+
+	//Resize user profile pic to 50 X 50
+	src, err := imaging.Open("1.jpg")
+	if err != nil{
+	  log.Fatal("Error %v", err)
+	}
+
+	src = imaging.Fill(src,50,50, imaging.Center, imaging.Lanczos)
+	err = imaging.Save(src,"profilepic.jpg")
+	if err != nil{
+	  log.Fatal("Error %v",err)
+	}
+ 
+	//Convert profile pic to circle
+	//open profile pic
 	profilepic, err := os.Open("profilepic.jpg")
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	img1, err := png.Decode(backGround)
-	img2, err := jpeg.Decode(profilepic)
-
+	profilepicd, err := jpeg.Decode(profilepic)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	//create canvas for profile pic
+	profilepiccanvas := image.NewRGBA(image.Rect(0,0,50,50))
+	p := image.Pt(25,25)
+	//draw mask on profile pic
+	draw.DrawMask(profilepiccanvas, profilepiccanvas.Bounds(), profilepicd, image.ZP, &circle{p, 25}, image.ZP, draw.Over)
+
+	//save as circle cropped 
+	pout, err := os.Create("./roundpic.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	png.Encode(pout, profilepiccanvas) 
+
+
+	//Merge profile pic on white background
+	backGround, err := os.Open("background.png")
+	roundpic, err := os.Open("roundpic.png")
+	backGroundd, err := png.Decode(backGround)
+	roundpicd, err := png.Decode(roundpic)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Draw profilepic on white backgroud
 	offset := image.Pt(300, 200)
-    b := img1.Bounds()
+    b := backGroundd.Bounds()
     canvas := image.NewRGBA(b)
-    draw.Draw(canvas, b, img1, image.ZP, draw.Src)
-    draw.Draw(canvas, img2.Bounds().Add(offset), img2, image.ZP, draw.Over)
+    draw.Draw(canvas, b, backGroundd, image.ZP, draw.Src)
+    draw.Draw(canvas, roundpicd.Bounds().Add(offset), roundpicd, image.ZP, draw.Over)
 
-	out, err := os.Create("./reset.jpg")
+	//Write text on Image
+	//set font color
+	col := color.RGBA{0, 0, 0, 255}
+	AddLabel(canvas, 360, 215, "Bharat Sewani",inconsolata.Bold8x16,col)
+	col = color.RGBA{0, 0, 0, 150}
+	AddLabel(canvas, 360, 230, "@bharatsewani199",inconsolata.Regular8x16,col)
+	col = color.RGBA{0, 0, 0, 255}
+	AddLabel(canvas, 310, 270, "No matter how far you have gone on the wrong road,",inconsolata.Bold8x16,col)
+	AddLabel(canvas, 310, 290, "You can still turn around.",inconsolata.Bold8x16,col)
+	col = color.RGBA{27,149,224,255}
+	AddLabel(canvas, 310, 310, "#TuesdayMotivation",inconsolata.Bold8x16,col)
+	col = color.RGBA{0, 0, 0, 150}
+	AddLabel(canvas, 310, 340, "9:00 AM . Jul 31, 2018",inconsolata.Regular8x16,col)
+
+//	fmt.Println(reflect.TypeOf(inconsolata.Regular8x16))
+	
+
+	//save frame
+	out, err := os.Create("./frame.jpg")
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	var opt jpeg.Options
 	opt.Quality = 80
-
 	jpeg.Encode(out, canvas, &opt) 
+
 }
 
 func main() {
